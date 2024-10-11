66a70312d8fa73dbf762ef1c0dba9e55f7253af0
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2024-10-11 10:17:31 -0400

    tour info

--- js/main.js
+++ js/main.js
@@ -8,8 +8,21 @@ $(function () {
 
     fixedContentPos: false,
   });
+
   $(".gallery__slider").slick({
     prevArrow: '<img class="prev-arrow" src="images/arrow-left.svg" alt="">',
     nextArrow: '<img class="next-arrow" src="images/arrow-right.svg" alt="">',
+  });
+
+  $(".gallery__item-inner").magnificPopup({
+    delegate: "a",
+    type: "image",
+    tLoading: "Loading image #%curr%...",
+    mainClass: "mfp-img-mobile",
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1], // Will preload 0 - before current, and 1 after the current image
+    },
   });
 });
